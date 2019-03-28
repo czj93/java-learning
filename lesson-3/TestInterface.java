00364a3fa3da890d6a5b29f478440c906d0f483f
曹志剑 <caozhijian@caozhijiandeMac-mini.local>
2019-03-28 20:37:37 +0800

    interface

--- lesson-3/TestInterface.java
+++ lesson-3/TestInterface.java
@@ -11,7 +11,9 @@ interface A{
 }
 
 class B implements A{
-	void m1(){
+	// interface 中的方法都是 public abstract, 所以 java 可以忽略这些修饰词
+	// 子类继承 实现接口的时候 方法需要加上， 否则编译报错
+	public void m1(){
 		System.out.println("b m1");
 	}
 }
